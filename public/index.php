58254b0b1fb1a451845a108a5945f69f04726355
Ivan Dev <g.ivan@dizboard.com>
2016-04-12 18:20:11 +0300

    del hard config

--- public/index.php
+++ public/index.php
@@ -4,9 +4,6 @@ namespace SiteDezz;
 
 use Dez\Config\Config;
 
-error_reporting(1);
-ini_set('display_errors', 1);
-
 include_once '../app/vendor/autoload.php';
 include_once '../app/SiteDezzApplication.php';
 
